f785d0ab466121ff3c9e20934a05fcec198a21e8
PedroAnjosPS <phpsanches@gmail.com>
2026-01-26 23:55:03 -0300

    User update using PUT

--- src/main/java/com/pedroanjosps/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroanjosps/workshopmongo/services/UserService.java
@@ -36,6 +36,20 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User user) {
+		User newUser = repo.findById(user.getId())
+				.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+		
+		updateData(newUser, user);
+		
+		return repo.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
