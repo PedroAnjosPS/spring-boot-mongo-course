acb199c3b9344de8707c1bb36e314a36de4b1f95
PedroAnjosPS <phpsanches@gmail.com>
2026-01-26 21:21:10 -0300

    Retrieving a user by ID

--- src/main/java/com/pedroanjosps/workshopmongo/services/UserService.java
+++ src/main/java/com/pedroanjosps/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.pedroanjosps.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedroanjosps.workshopmongo.domain.User;
 import com.pedroanjosps.workshopmongo.repository.UserRepository;
+import com.pedroanjosps.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
 	}
 }
